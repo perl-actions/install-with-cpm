cf7f198f8fc3f5d10670abd605a8c009e8c07524
Nicolas R <cpan@atoomic.org>
2020-04-13 11:23:21 -0600

    add actions/exec

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
 const tc = require('@actions/tool-cache');
+const exec = require('@actions/exec');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -13,11 +13,14 @@ try {
   // 
   const cpmScript = await tc.downloadTool('https://git.io/cpm');
 
+  await exec.exec('perl', [ cpmScript, 'install', '-g' 'App::cpm'] );
+
   core.setOutput("cpm", cpmScript);
+  //core.addPath(nodeDirectory);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
