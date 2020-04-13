1cc9522b7d007a5f4ab3494ac278ae5727373002
Nicolas R <cpan@atoomic.org>
2020-04-13 11:16:04 -0600

    add and use actions/exec

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const tc = require('@actions/tool-cache');
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+
+  // 
+  const cpmScript = await tc.downloadTool('https://git.io/cpm');
+
+  core.setOutput("cpm", cpmScript);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
