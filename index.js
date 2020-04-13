75b15a15690602020870ce83ea2c8ffcdd621305
Nicolas R <cpan@atoomic.org>
2020-04-13 11:44:42 -0600

    cleanup

--- index.js
+++ index.js
@@ -4,23 +4,9 @@ const tc = require('@actions/tool-cache');
 const exec = require('@actions/exec');
 
 async function action() {
-
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-
   const cpmScript = await tc.downloadTool('https://git.io/cpm');
-
   core.setOutput("cpm", cpmScript);
   await exec.exec( 'sudo', [ 'perl', cpmScript, 'install', '-g', 'App::cpm'] );
-
-  //core.addPath(nodeDirectory);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
-
   return;
 }
 
