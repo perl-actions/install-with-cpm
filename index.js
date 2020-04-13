72cca8d015f8ba6b60a5beb0064db409353f9d78
Nicolas R <cpan@atoomic.org>
2020-04-13 11:35:17 -0600

    use async function

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const github = require('@actions/github');
 const tc = require('@actions/tool-cache');
 const exec = require('@actions/exec');
 
-try {
-  // `who-to-greet` input defined in action metadata file
+async function action() {
+
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
-  // 
   const cpmScript = await tc.downloadTool('https://git.io/cpm');
 
-  await exec.exec('perl', [ cpmScript, 'install', '-g' 'App::cpm'] );
-
   core.setOutput("cpm", cpmScript);
+  await exec.exec('perl', [ cpmScript, 'install', '-g', 'App::cpm'] );
+
   //core.addPath(nodeDirectory);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+  return;
 }
+
+// Call start
+(async() => {
+  try {
+  	await action();	
+  } catch (error) {
+  	core.setFailed(error.message);	
+  }
+})();
